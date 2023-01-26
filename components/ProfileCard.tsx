aa78bd61b623e72a322a2620a3a76a9f7ea8233f
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-01-26 16:12:18 +0100

    Add profile card component.

---
+++ components/ProfileCard.tsx
@@ -0,0 +1,153 @@
+// Get profile information from Strapi and display it in the ProfileCard component
+
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import {
+  MdPhoneIphone,
+  MdLocationOn,
+  MdEmail,
+  MdCalendarToday,
+  MdDownload,
+} from "react-icons/md";
+import { FaLinkedin, FaGithub, FaTwitter, FaFacebook } from "react-icons/fa";
+import { useEffect, useState } from "react";
+
+interface ProfileCardProps {
+  profile: any;
+  env: string;
+}
+
+const ProfileCard: React.FC<ProfileCardProps> = ({ profile, env }) => {
+	/*
+  const formatDate = (date: string) => {
+    return new Date(date).toLocaleDateString("fr-FR");
+  };
+
+  const getAge = (date: string) => {
+    const today = new Date();
+    const birthDate = new Date(date);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  };
+	*/
+
+  const birthday = ` ans`;
+
+  return (
+    <div className="col-span-12 lg:col-span-4 lg:h-screen lg:sticky top-44">
+      <div className="w-full mb-6 lg:mb-0 mx-auto relative bg-white text-center dark:bg-[#111111] px-6 rounded-[20px] mt-[180px] md:mt-[220px] lg:mt-0 ">
+        <Image
+          loader={({ src }) => src}
+          src={`${env}${profile.avatar.data.attributes.url}`}
+          width={profile.avatar.data.attributes.formats.thumbnail.width}
+          height={profile.avatar.data.attributes.formats.thumbnail.height}
+          title={profile.avatar.data.attributes.caption || null}
+          alt={profile.avatar.data.attributes.alternativeText || null}
+          className="w-[240px] absolute left-[50%] transform -translate-x-[50%] h-[240px] drop-shadow-xl mx-auto rounded-[20px] -mt-[140px]"
+        />
+        <div className="pt-[100px] pb-8">
+          <h1 className="mt-6 mb-1 text-5xl font-semibold dark:text-white">
+            {profile.title}
+          </h1>
+          <p className="mb-4 text-[#7B7B7B] inline-block dark:bg-[#1D1D1D] px-5 py-1.5 rounded-lg dark:text-[#A6A6A6] ">
+            {profile.sub_title}
+          </p>
+          <div className="flex justify-center space-x-3">
+            {profile.links.map((link: any) => (
+              <Link
+                href={link.link}
+                title={link.text}
+                {...(link.isBlank && { target: "_blank" })}
+								key={link.id}
+              >
+                <span className="socialbtn text-[#44566C] dark:text-[#A6A6A6]">
+                  <FaGithub />
+                </span>
+              </Link>
+            ))}
+          </div>
+          <div className="p-7 rounded-2xl mt-7 bg-[#F3F6F6] dark:bg-[#1D1D1D]">
+            <div className="flex py-2.5 border-b border-[#E3E3E3] dark:border-[#3D3A3A]">
+              <span className="flex-shrink-0 socialbtn bg-white dark:bg-black text-[#E93B81] shadow-md">
+                <MdPhoneIphone />
+              </span>
+              <div className="text-left ml-2.5">
+                <p className="text-xs text-[#44566C] dark:text-[#A6A6A6]">
+                  Phone
+                </p>
+                <p className="dark:text-white break-all">
+                  <Link
+                    href={`tel:+${profile.phone}`}
+                    title="Téléphone"
+                    className="hover:text-[#FA5252] duration-300 transition"
+                  >
+                    {`+${profile.phone}`}
+                  </Link>
+                </p>
+              </div>
+            </div>
+            <div className="flex py-2.5 border-b border-[#E3E3E3] dark:border-[#3D3A3A]">
+              <span className="flex-shrink-0 socialbtn bg-white dark:bg-black text-[#6AB5B9]  shadow-md">
+                <MdLocationOn />
+              </span>
+              <div className="text-left ml-2.5">
+                <p className="text-xs text-[#44566C] dark:text-[#A6A6A6]">
+                  Location
+                </p>
+                <p className="dark:text-white break-all">{profile.location}</p>
+              </div>
+            </div>
+            <div className="flex py-2.5 border-b border-[#E3E3E3] dark:border-[#3D3A3A]">
+              <span className="flex-shrink-0 socialbtn bg-white dark:bg-black text-[#FD7590] shadow-md">
+                <MdEmail />
+              </span>
+              <div className="text-left ml-2.5">
+                <p className="text-xs text-[#44566C] dark:text-[#A6A6A6]">
+                  Email
+                </p>
+                <p className="dark:text-white break-all">
+                  <Link href={`mailto:${profile.mail}`} title="Email">
+                    {profile.mail}
+                  </Link>
+                </p>
+              </div>
+            </div>
+            <div className="flex py-2.5">
+              <span className="flex-shrink-0 socialbtn bg-white dark:bg-black text-[#C17CEB] shadow-md">
+                <MdCalendarToday />
+              </span>
+              <div className="text-left ml-2.5">
+                <p className="text-xs text-[#44566C] dark:text-[#A6A6A6]">
+                  Age
+                </p>
+                <p className="dark:text-white break-all">{birthday}</p>
+              </div>
+            </div>
+          </div>
+          <Link
+            href={`${env}${
+              profile.resume && profile.resume.data.attributes.url
+            }`}
+            title={
+              (profile.resume && profile.resume.data.attributes.name) || null
+            }
+            target="_blank"
+            type={profile.resume && profile.resume.data.attributes.mime}
+            className="inline-flex items-center mx-auto bg-gradient-to-r from-[#FA5252] to-[#DD2476] duration-200 transition ease-linear hover:bg-gradient-to-l from-[#DD2476] to-[#fa5252ef] px-8 py-3 text-lg text-white rounded-[35px] mt-6"
+          >
+            <MdDownload className="mr-2" />
+            {(profile.resume && profile.resume.data.attributes.caption) ||
+              "null"}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,31 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
-import Image from 'next/image'
-import { css } from '@emotion/react'
-import Link from 'next/link'
+import Head from "next/head";
+import { Inter } from "@next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+import ProfileCard from "@/components/ProfileCard";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 interface Props {
-  data: any
-  error: any
-  body: any
-  school: any
-  professional_experience: any
-  env: string
-  meta: any
+  data: any;
+  error: any;
+  body: any;
+  school: any;
+  professional_experience: any;
+  profile: any;
+  env: string;
+  meta: any;
 }
 
-const Home = ({ data, body, school, professional_experience, env, meta }: Props) => {
+const Home = ({
+  data,
+  body,
+  school,
+  professional_experience,
+  profile,
+  env,
+  meta,
+}: Props) => {
   return (
     <>
       <Head>
@@ -29,91 +38,78 @@ const Home = ({ data, body, school, professional_experience, env, meta }: Props)
         <meta name="author" content={meta.meta_author} />
         <meta name="robots" content={meta.meta_robots} />
       </Head>
-      
-      <main>
-        <Image
-          loader={({ src }) => src}
-          src={`${env}${data.attributes.background_image.data.attributes.url}`}
-          width={data.attributes.background_image.data.attributes.width}
-          height={data.attributes.background_image.data.attributes.height}
-          title={data.attributes.background_image.data.attributes.caption || null }
-          alt={data.attributes.background_image.data.attributes.alternativeText || null} />
-        <Image
-          loader={({ src }) => src}
-          src={`${env}${data.attributes.profile_picture.data.attributes.url}`}
-          width={data.attributes.profile_picture.data.attributes.width}
-          height={data.attributes.profile_picture.data.attributes.height}
-          alt={data.attributes.profile_picture.data.attributes.alternativeText || null}
-          title={data.attributes.profile_picture.data.attributes.caption || null} 
-          className="profile-picture" />
-        <h1>{data.attributes.title}</h1>
-        <p>{data.description}</p>
-        <div>
-          {data.attributes.sub_title.map((subtitle: any) => (
-            <p>{subtitle.sub_title}</p>
+
+      <main className="bg-homeBg min-h-screen bg-no-repeat bg-center bg-cover bg-fixed dark:bg-homeTwoBg-dark md:pb-16 w-full">
+        <div className="container mx-auto grid grid-cols-12 md:gap-10 justify-between lg:mt-[220px]">
+          <ProfileCard profile={profile} env={env} />
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            {data.attributes.title}
+          </h1>
+          <p>{data.description}</p>
+          <h2>{body.title}</h2>
+          {body.icon && <p>{body.icon}</p>}
+          <p>{body.body}</p>
+          <Link
+            href={`${env}${body.resume.data.attributes.url}`}
+            title={body.resume.data.attributes.name || null}
+          >
+            {body.title_resume}
+          </Link>
+          <h2>{data.attributes.school_title}</h2>
+          {school.map((school: any) => (
+            <div key={school.id}>
+              <h3>{school.school_name}</h3>
+              <p>{school.start_date}</p>
+              <p>{school.end_date || "Present"}</p>
+              <p>{school.grade_name}</p>
+              {school.comment.map((comment: any) => (
+                <p key={comment.id}>{comment.comment}</p>
+              ))}
+            </div>
+          ))}
+          <h2>{data.attributes.professional_experience_title}</h2>
+          {professional_experience.map((professional_experience: any) => (
+            <div key={professional_experience.id}>
+              <h3>{professional_experience.company_name}</h3>
+              <p>{professional_experience.start_date}</p>
+              <p>{professional_experience.end_date || "Present"}</p>
+              <p>{professional_experience.job_title}</p>
+              {professional_experience.comment}
+            </div>
           ))}
         </div>
-        <h2>
-          {body.title}
-        </h2>
-        {body.icon && <p>{body.icon}</p>}
-        <p>{body.body}</p>
-        <Link
-          href={`${env}${body.resume.data.attributes.url}`}
-          title={body.resume.data.attributes.name || null}
-        >
-          {body.title_resume}
-        </Link>
-        <h2>
-          {data.attributes.school_title}
-        </h2>
-        {school.map((school: any) => (
-          <div>
-            <h3>{school.school_name}</h3>
-            <p>{school.start_date}</p>
-            <p>{school.end_date || 'Present'}</p>
-            <p>{school.grade_name}</p>
-            {school.comment.map((comment: any) => (
-              <p>{comment.sub_title}</p>
-            ))}
-          </div>
-        ))}
-        <h2>
-          {data.attributes.professional_experience_title}
-        </h2>
-        {professional_experience.map((professional_experience: any) => (
-          <div>
-            <h3>{professional_experience.company_name}</h3>
-            <p>{professional_experience.start_date}</p>
-            <p>{professional_experience.end_date || 'Present'}</p>
-            <p>{professional_experience.job_title}</p>
-            {professional_experience.comment}
-          </div>
-        ))}
       </main>
     </>
-  )
-}
+  );
+};
 
 // Get data from Strapi with the token from the environment variable
 Home.getInitialProps = async () => {
-  const query = `?populate[]=profile_picture&populate[]=background_image&populate[]=sub_title&populate[]=body.resume&populate[]=school.comment&populate[]=professional_experience&populate[]=meta`
+  const query = `?populate[]=profile_picture&populate[]=background_image&populate[]=sub_title&populate[]=body.resume&populate[]=school.comment&populate[]=professional_experience&populate[]=meta&populate[]=profile.links&populate[]=profile.avatar&populate[]=profile.resume`;
   const res = await fetch(`${process.env.API_URL}/api/homepage${query}`, {
     headers: {
       Authorization: `Bearer ${process.env.API_TOKEN}`,
     },
-  })
-  const data = await res.json()
-  const error = data.error || null
-  const meta = data.data.attributes.meta
-  const body = data.data.attributes.body
-  const school = data.data.attributes.school
-  const professional_experience = data.data.attributes.professional_experience
-  const env = process.env.API_URL
+  });
+  const data = await res.json();
+  const error = data.error || null;
+  const meta = data.data.attributes.meta;
+  const body = data.data.attributes.body;
+  const school = data.data.attributes.school;
+  const professional_experience = data.data.attributes.professional_experience;
+  const profile = data.data.attributes.profile;
+  const env = process.env.API_URL;
 
-  console.log(meta)
+  return {
+    data: data.data,
+    error: error,
+    body: body,
+    school: school,
+    professional_experience: professional_experience,
+    profile: profile,
+    env: env,
+    meta: meta,
+  };
+};
 
-  return { data: data.data, error: error, body: body, school: school, professional_experience: professional_experience, env: env, meta: meta }
-}
-
-export default Home
+export default Home;
